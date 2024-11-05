07e201a111f8a29e9fec864ce352541568a0383d
Sharit-Vargas07 <sharitvargas07@gmail.com>
2024-11-05 18:37:55 -0500

    backend

---
+++ backend/src/routes/seguimiento.route.js
@@ -0,0 +1,35 @@
+import Router from 'express'
+import { listarSeguimiento, registrarSeguimiento, actualizarSeguimiento, cargarSeguimiento, aprobarSeguimiento, rechazarSeguimiento, listarSeguimientoAprendices, uploadPdfToSeguimiento, descargarPdf, listarEstadoSeguimiento, listarEstadosBitacorasSeguimientos } from '../controllers/seguimientos.controller.js'
+import { validarToken } from './../controllers/seguridad.controller.js'
+
+const rutaSeguimiento = Router()
+
+rutaSeguimiento.get('/listar',validarToken,  listarSeguimiento)
+rutaSeguimiento.get('/listarA', validarToken,   listarSeguimientoAprendices)
+rutaSeguimiento.post('/registrar', validarToken, cargarSeguimiento, registrarSeguimiento)
+rutaSeguimiento.post('/cargarPdf/:id_seguimiento', validarToken, cargarSeguimiento, uploadPdfToSeguimiento)
+rutaSeguimiento.get('/estadosBitacoras/:id_persona', validarToken,  listarEstadosBitacorasSeguimientos)
+rutaSeguimiento.put('/actualizar/:id', validarToken, cargarSeguimiento, actualizarSeguimiento)
+rutaSeguimiento.put('/aprobar/:id_seguimiento', validarToken, aprobarSeguimiento)
+rutaSeguimiento.put('/rechazar/:id_seguimiento', validarToken, rechazarSeguimiento)
+rutaSeguimiento.get('/descargarPdf/:id_seguimiento',  descargarPdf)
+rutaSeguimiento.get('/listarEstado/:id_seguimiento', validarToken, listarEstadoSeguimiento)
+
+
+
+export default rutaSeguimiento
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
