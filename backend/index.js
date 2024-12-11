30d51068156bac2b97e9f128d16e47cf7acd6b37
Sharit-Vargas07 <sharitvargas07@gmail.com>
2024-12-11 17:29:42 -0500

    movil

--- backend/index.js
+++ backend/index.js
@@ -63,6 +63,6 @@ servidor.get('/document', (req,res) => {
     res.render('document.ejs')
 })
 
-servidor.listen(3000, '0.0.0.0', () => {
+servidor.listen(3000, '192.168.0.102', () => {
     console.log('Servidor funcionando en el puerto 3000');
 });
